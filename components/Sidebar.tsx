378650577ced0fe260030a47914f68bc56f1e638
Võ Phước Thạnh <92651849+vophuocthanh@users.noreply.github.com>
2024-04-03 15:19:46 +0700

    update ver new

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,8 +13,9 @@ const Sidebar = () => {
     <section className="sticky left-0 top-0 flex h-screen w-fit flex-col  justify-between  bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
       <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((item) => {
-          const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`);
-          
+          const isActive =
+            pathname === item.route || pathname.startsWith(`${item.route}/`);
+
           return (
             <Link
               href={item.route}
@@ -23,7 +24,7 @@ const Sidebar = () => {
                 'flex gap-4 items-center p-4 rounded-lg justify-start',
                 {
                   'bg-blue-1': isActive,
-                }
+                },
               )}
             >
               <Image
