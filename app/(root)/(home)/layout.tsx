a72a4703edcd26fff2195af6e495470cc60e6098
Võ Phước Thạnh <92651849+vophuocthanh@users.noreply.github.com>
2024-04-03 14:25:05 +0700

    update title

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -5,18 +5,18 @@ import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 
 export const metadata: Metadata = {
-  title: 'YOOM',
+  title: 'ZOOM',
   description: 'A workspace for your team, powered by Stream Chat and Clerk.',
 };
 
-const RootLayout = ({ children }: Readonly<{children: ReactNode}>) => {
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
     <main className="relative">
       <Navbar />
 
       <div className="flex">
         <Sidebar />
-        
+
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
           <div className="w-full">{children}</div>
         </section>
